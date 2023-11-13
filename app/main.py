e8864eb7e82d9b90dd0918ee8e8e3ac9ec4c0399
KarMiguel <carlosmiguel.dsa12@gmail.com>
2023-11-13 15:20:39 -0300

    add bd user

--- app/main.py
+++ app/main.py
@@ -4,13 +4,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-<<<<<<< HEAD
 @app.get("/")
 def health_check():
     return {"status": "ok"}
 
-=======
-@app.get("/health-check")
-def health_check():
-    return {"status": "ok"}
->>>>>>> 7460434a6a0601a56eac8a201d9257c754c49816
